0042515dac5e6b45a15bbd1b100a310dc41a63ee
Roman Podymov <podymfrombryansk@yandex.com>
2024-09-19 18:03:27 +0200

    Initial source code

---
+++ CurrencyConverter/AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        setupWindow()
+        return true
+    }
+
+    private func setupWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MyViewController()
+        window?.makeKeyAndVisible()
+    }
+}
