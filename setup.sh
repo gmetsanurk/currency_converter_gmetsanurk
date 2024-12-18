2b8200915c6931d74104ace603505b98a55abdaa
Roman Podymov <podymfrombryansk@yandex.com>
2024-12-18 15:47:22 +0100

    Script improvements

--- setup.sh
+++ setup.sh
@@ -5,33 +5,28 @@ echo "Starting project setup..."
 # Install Mint
 if command -v mint >/dev/null; then
     echo "Bootstrapping Mint packages..."
-    mint bootstrap && mint run xcodegen || { echo "Error: Failed to generate Xcode project."; exit 1; }
+    mint bootstrap || { echo "Error: Failed to install Mint dependencies"; exit 2; }
 else
     echo "Error: Mint is not installed. Please install Mint (https://github.com/yonaskolb/Mint)."
     exit 1
 fi
 
 # Generate Xcode project
-if command -v xcodegen >/dev/null; then
-    echo "Generating Xcode project..."
-    xcodegen
-else
-    echo "Error: XcodeGen is not installed. Please install XcodeGen (https://github.com/yonaskolb/XcodeGen)."
-    exit 1
-fi
+echo "Generating Xcode project..."
+mint run xcodegen || { echo "Error: Cannot generate Xcode project"; exit 3; }
 
 # Install Bundler
 if command -v bundle >/dev/null; then
     echo "Bundler is already installed."
 else
     echo "Installing Bundler..."
-    gem install bundler || { echo "Error: Failed to install Bundler."; exit 1; }
+    gem install bundler || { echo "Error: Failed to install Bundler."; exit 4; }
 fi
 
 # Bundle install
 if [ -f "Gemfile" ]; then
     echo "Installing Ruby gems..."
-    bundle install || { echo "Error: Failed to run bundle install."; exit 1; }
+    bundle install || { echo "Error: Failed to run bundle install."; exit 5; }
 else
     echo "Gemfile not found. Skipping bundle install."
 fi
@@ -39,7 +34,7 @@ fi
 # Install CocoaPods
 if [ -f "Podfile" ]; then
     echo "Installing CocoaPods dependencies..."
-    bundle exec pod install || { echo "Error: Failed to install CocoaPods dependencies."; exit 1; }
+    bundle exec pod install || { echo "Error: Failed to install CocoaPods dependencies."; exit 6; }
 else
     echo "Podfile not found. Skipping CocoaPods dependencies installation."
 fi
