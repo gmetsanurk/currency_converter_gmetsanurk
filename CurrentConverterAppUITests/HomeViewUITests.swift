8c5357d650efbe293e7e483ee09c03f46a3eb740
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-03-25 14:28:49 +0300

    Begin to code for tests with targets

--- CurrentConverterAppUITests/HomeViewUITests.swift
+++ CurrentConverterAppUITests/HomeViewUITests.swift
@@ -1,6 +1,6 @@
 import XCTest
 
-final class CurrentConverterAppUITests: XCTestCase {
+final class HomeViewUITests: XCTestCase {
     override func setUpWithError() throws {
         continueAfterFailure = false
         // XCUIApplication().launch()
